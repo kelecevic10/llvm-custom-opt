f8460df52def84a50f747c373909bfe597345390
nemanja <kelecevicnemanja@gmail.com>
2025-10-24 17:04:28 +0200

    Created separate script for generating .ll files

---
+++ generate_ll.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# Path to LLVM build folder
+LLVM_BUILD="/home/nemanja/Desktop/Konstrukcija Kompilatora/LLVM/llvm-project/build"
+
+rm -f tests/*.ll
+"$LLVM_BUILD/bin/clang" -emit-llvm -S tests/input.cpp -o tests/input.ll
